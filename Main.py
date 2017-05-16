15ee5a1d3f0d6cedc33f6af946cf6e1964083d35
SeoJinhye <jyoon@sjnma.org>
2017-05-16 11:03:51 -0500

    [I hope Final commit]

--- Main.py
+++ Main.py
@@ -32,11 +32,13 @@ def Compare(Player, CPU):
 gane = Main()
 CPUS = 10000
 PlayerS = 10000
+PlayerN = raw_input("What is your name?")
+print("Hello " + PlayerN)
 
 while(CPUS > 0 or PlayerS > 0):
 	start = raw_input("Press s to begin the game")
 	if(start == "s"):
-		print("Current Player money: " + str(PlayerS) + "Current CPU money: " + str(CPUS))
+		print("Current " + PlayerN + " money: " + str(PlayerS) + "Current CPU money: " + str(CPUS))
 		Bet = int(input("How much do you want to bet?"))
 		P =gane.Player()
 		C = gane.CPU()
@@ -47,7 +49,7 @@ while(CPUS > 0 or PlayerS > 0):
 				CPUS += Bet
 				PlayerS -= Bet
 			if(Compare(P,C) == 0):
-				print("Player wins!")
+				print(PlayerN + "wins!")
 				CPUS -= Bet
 				PlayerS += Bet
 			elif(Compare(P,C) == 2):
@@ -60,7 +62,7 @@ while(CPUS > 0 or PlayerS > 0):
 				CPUS += Bet
 				PlayerS -= Bet
 			if(Compare(P,C) == 0):
-				print("Player wins!")
+				print(PlayerN+ " wins!")
 				CPUS -= Bet
 				PlayerS += Bet
 			elif(Compare(P,C) == 2):
@@ -70,8 +72,8 @@ while(CPUS > 0 or PlayerS > 0):
 	if(CPUS <= 0 or PlayerS <= 0):
 		break
 if(CPUS > PlayerS):
-	print("You lost all the money You lost!")
+	print(PlayerN+ "lost all the money You lost!")
 if(CPUS < PlayerS):
-	print("CPU lost all the money You Win! Buy a flight to Las Vegas!")
+	print("CPU lost all the money You Win!")
 if(CPUS == PlayerS):
 	print("Draw!")
